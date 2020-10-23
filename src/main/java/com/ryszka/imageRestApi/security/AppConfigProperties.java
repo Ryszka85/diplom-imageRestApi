ffaa1a1d3f8701d7545dc564a25d9108ac422a35
Ryszka85 <49881651+Ryszka85@users.noreply.github.com>
2020-10-23 04:54:00 +0200

    Update AppConfigProperties.java

--- src/main/java/com/ryszka/imageRestApi/security/AppConfigProperties.java
+++ src/main/java/com/ryszka/imageRestApi/security/AppConfigProperties.java
@@ -4,7 +4,7 @@ public class AppConfigProperties {
     public static final String HOST = "127.0.0.1";
     public static final String USER = "Admin";
     public static final String PASSWORD = "Admin";
-    public static final String GOOGLE_ID = "200318745451-u9viua8onpt25sdmknka7i89uhdrknsd.apps.googleusercontent.com";
+    public static final String GOOGLE_ID = "YOUR ID";
     public static final String FILE_PATH = "https://storage.googleapis.com/htlimagerestapi.appspot.com/"; //"http://localhost/files/";
     public static final String FIREBASE_PROJECT_ID = "htlimagerestapi";
     public static final String FIRE_BASE_CONFIG_FILE_PATH = "C:\\Users\\planb\\programing\\Java\\imageRestApi\\htlimagerestapi-firebase-adminsdk-3tlwm-ecc66cd433.json";
